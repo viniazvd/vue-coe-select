eba888a9185b763525cb214175665addbc2a2fd3
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-28 09:53:52 -0300

    init

---
+++ bili.config.js
@@ -0,0 +1,15 @@
+const { name } = require('./package.json')
+const vue = require('rollup-plugin-vue').default
+const css = require('rollup-plugin-css-only')
+
+module.exports = {
+  js: 'buble',
+  input: 'src/index.js',
+  banner: true,
+  format: ['umd', 'umd-min', 'cjs', 'es'],
+  filename: name + '[suffix].js',
+  plugins: [
+    css({ output: `dist/${name}.css` }),
+    vue({ css: false })
+  ]
+}
