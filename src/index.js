900e5ec952363559aafb4f480139b7c368c5d938
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-28 09:54:05 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import VueCoeSelect from './components/VueCoeSelect.vue'
+import pointer from './mixins/pointer.js'
+import searchable from './mixins/searchable'
+
+export default VueCoeSelect
+
+export { VueCoeSelect, pointer, searchable }
