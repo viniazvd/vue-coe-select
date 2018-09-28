30bb46fb4c0837e1ccc27f92e4cdc261efe22793
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-28 10:50:32 -0300

    build config

--- bili.config.js
+++ bili.config.js
@@ -1,14 +1,16 @@
-// import css from 'rollup-plugin-css-only'
+const { name } = require('./package.json')
+
 const vue = require('rollup-plugin-vue').default
+const css = require('rollup-plugin-css-only')
 
 module.exports = {
+  js: 'buble',
   input: 'src/index.js',
-  output: {
-    format: 'esm',
-    file: 'dist/vue-coe-select.esm.js'
-  },
+  banner: true,
+  format: ['umd', 'umd-min', 'cjs', 'es'],
+  filename: name + '[suffix].js',
   plugins: [
-    // css(),
-    vue()
+    css({ output: `dist/${name}.css` }),
+    vue({ css: false })
   ]
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-// import VueCoeSelect from './components/VueCoeSelect.vue'
-// import pointer from './mixins/pointer.js'
-// import searchable from './mixins/searchable'
-
-// export default VueCoeSelect
-
-// export { VueCoeSelect, pointer, searchable }
-
 import VueCoeSelect from './components/VueCoeSelect.vue'
 
 if (typeof Vue !== 'undefined') {
