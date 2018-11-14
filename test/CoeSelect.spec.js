53b1522bc99bcadddb903ab024cafd2182396b60
Vinicius Azevedo <viniazvd@gmail.com>
2018-11-14 00:18:51 -0200

    improve tests

--- test/CoeSelect.spec.js
+++ test/CoeSelect.spec.js
@@ -26,25 +26,78 @@ describe('CoeSelect', () => {
     expect (wrapper.findAll('label').exists()).toBe(true)
   })
 
+  test('required', () => {
+    const wrapper = shallowMount(CoeSelect, { propsData: { items, required: true } })
+
+    expect (wrapper.vm.required).toBe(true)
+  })
+
   test('placeholder', () => {
     const wrapper = shallowMount(CoeSelect, { propsData: { items, placeholder: 'coe' } })
 
     expect (wrapper.vm.searchableValue[0]).toBe('coe')
   })
 
-  // test('open', () => {
-  //   const wrapper = mount(CoeSelect, { propsData: { items } })
+  test('isOpened', () => {
+    const wrapper = mount(CoeSelect, { propsData: { items } })
 
-  //   wrapper.trigger('click')
+    const input = wrapper.find('.input')
+    input.trigger('click')
 
-  //   expect(wrapper.vm.isOpened).toBe(true)
+    expect(wrapper.vm.isOpened).toBe(true)
+  })
+
+  test('isFocused', () => {
+    const wrapper = mount(CoeSelect, { propsData: { items, clearOnSelect: true } })
+
+    const input = wrapper.find('.input')
+    input.trigger('click')
+
+    expect(wrapper.vm.focused && wrapper.vm.clearOnSelect).toBe(true)
+  })
+
+  // test('items length', () => {
+  //   const wrapper = shallowMount(CoeSelect, { propsData: { items } })
+  //   const input = wrapper.find('.input')
+  //   input.trigger('click')
+
+  //   expect (wrapper.find('.items')).toHaveLength(items.length)
   // })
 
-  // test('items length', async () => {
-  //   const wrapper = shallowMount(CoeSelect, { propsData: { items, value: 'coe1' } })
-  //   wrapper.setData({ isOpened: true })
-  //   // console.log(wrapper.vm.$data.isOpened)
+  test('pointer 1', () => {
+    const wrapper = shallowMount(CoeSelect, { propsData: { items } })
 
-  //   expect (wrapper.findAll('item')).toHaveLength(items.length)
-  // })
+    const input = wrapper.find({ ref: 'searchable' })
+    // default pointer: -1
+    input.trigger('keydown.down') // 0
+    input.trigger('keydown.down') // 1
+    input.trigger('keydown.down') // 2
+
+    expect(wrapper.vm.pointer).toBe(2)
+  })
+
+  test('is multiple', () => {
+    const wrapper = shallowMount(CoeSelect, {
+      propsData: {
+        items,
+        value: {}, // verificar pq value prcisa ser um obj
+        multiple: true
+      }
+    })
+
+    expect(wrapper.props('multiple')).toBe(true)
+  })
+
+  test('validation', () => {
+    const wrapper = shallowMount(CoeSelect, {
+      propsData: {
+        items,
+        value: {}, // verificar pq value prcisa ser um obj
+        multiple: true,
+        validation: items.length && `Máximo de 3 opções selecionadas`
+      }
+    })
+
+    expect(wrapper.vm.validation).toBe(`Máximo de 3 opções selecionadas`)
+  })
 })
